4e55b909ea0947fcf873e58aa60d23d2242b4f83
Harish Kailasa <hari.kailasa@gmail.com>
2023-04-21 05:19:37 +0530

    initial commit

---
+++ src/main/java/com/first/git/demo/ThirdDemo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.first.git.demo;
+
+/**
+ * @author Harish
+ *
+ */
+public class ThirdDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git");
+	}
+
+}
