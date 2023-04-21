713ea0474efdfd2e37b6b6c2c3eae2076e79a7dd
Harish Kailasa <hari.kailasa@gmail.com>
2023-04-21 05:32:19 +0530

    April Release changes

--- src/main/java/com/first/git/demo/ThirdDemo.java
+++ src/main/java/com/first/git/demo/ThirdDemo.java
@@ -14,7 +14,7 @@ public class ThirdDemo {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Git");
+		System.out.println("April Release");
 	}
 
 }
